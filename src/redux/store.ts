9a726ceffb08c189fbada5fb1971c70accf53c34
FeNRiR <germes955@gamail.com>
2022-01-26 01:24:53 +0300

    crate app and add lib

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import {userReducer} from "./reducers/userReducer";
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export type reducerState = ReturnType<typeof rootReducer>
