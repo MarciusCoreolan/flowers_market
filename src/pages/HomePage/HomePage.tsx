d08327926385e1ea0c7264cd8d35f552bd3df8bf
FeNRiR <germes955@gamail.com>
2022-01-27 20:15:34 +0300

    add catalog section on home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./home_page.module.scss";
+import Intro from "./Intro/Intro";
+import CatalogSection from "./CatalogSection/CatalogSection";
 
 function HomePage() {
   return (
     <div className={style.home_page}>
+      <Intro/>
+      <CatalogSection/>
     </div>
   );
 }
