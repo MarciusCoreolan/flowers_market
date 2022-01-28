0e9216dbdbe33a6859cd40bfda2d929cc4210dca
FeNRiR <germes955@gamail.com>
2022-01-28 17:25:03 +0300

    add db json and server js

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import style from "./home_page.module.scss";
 import Intro from "./Intro/Intro";
 import CatalogSection from "./CatalogSection/CatalogSection";
+import Popular from "./Popular/Popular";
 
 function HomePage() {
   return (
     <div className={style.home_page}>
       <Intro/>
       <CatalogSection/>
+      <Popular/>
     </div>
   );
 }
---
+++ src/pages/HomePage/Popular/Popular.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './popular.module.scss'
+
+function Popular() {
+    return (
+        <div className={style.popular_section}>
+ыыы
+        </div>
+    );
+}
+
+export default Popular;
